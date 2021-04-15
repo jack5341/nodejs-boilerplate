58881568d7d55c845dd158af88f56624cdd838f1
jack5341 <nedim.akar53411@gmail.com>
2021-04-15 20:48:41 +0200

    landing completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+import { terminal } from "terminal-kit"
+
+var items = [
+    'Express.js',
+    'Fastify.js',
+    'Without Framework'
+];
+
+terminal.white.bgBlack.bold.italic(' Node.js Boilerplate \n by https://github.com/jack5341 \n\n Choose a framework;')
+terminal.singleColumnMenu(items, function (error, response) {
+    terminal('\n').eraseLineAfter.green("Installing " + response.selectedText)
+    process.exit();
+});
