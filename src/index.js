064915203286c01486d6746f0b8bf6e35f986919
jack5341 <nedim.akar53411@gmail.com>
2021-04-15 21:47:06 +0200

    added filter function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { terminal } from "terminal-kit"
+import shell from "shelljs"
 
 var items = [
     'Express.js',
@@ -8,6 +9,28 @@ var items = [
 
 terminal.white.bgBlack.bold.italic(' Node.js Boilerplate \n by https://github.com/jack5341 \n\n Choose a framework;')
 terminal.singleColumnMenu(items, function (error, response) {
-    terminal('\n').eraseLineAfter.green("Installing " + response.selectedText)
+    terminal('\n').eraseLineAfter.green("Installing " + response.selectedText + "\n")
+    filterRepsonse(response.selectedIndex)
     process.exit();
 });
+
+function filterRepsonse(x) {
+    switch (x) {
+        case 0:
+            cloneBoi("git clone git@github.com:jack5341/ugur-anderungsschneiderei.git")
+            break;
+        case 1:
+            cloneBoi("git clone git@github.com:jack5341/ugur-anderungsschneiderei.git")
+            break;
+        case 2:
+            cloneBoi("git clone git@github.com:jack5341/ugur-anderungsschneiderei.git")
+            break;
+        default:
+            break;
+    }
+}
+
+function cloneBoi (url) {
+    shell.exec(url)
+    terminal.white("Your boilerplate is ready 😉")
+}
